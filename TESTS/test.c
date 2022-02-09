a606f0b1865f66d221a25ffcb877d8c3f0fa77e7
Leo Richard <lrichard@z3r4p1.42lyon.fr>
2022-02-09 18:30:58 +0100

    dossier de tests cree

---
+++ TESTS/test.c
@@ -0,0 +1,45 @@
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int lol = 1;
+
+void	sighandler(int sig)
+{
+	if (sig == SIGSTOP)
+	{
+		lol = 0;
+		write(1, "STOPPED\n", 8);
+	}
+	else if (sig == SIGCONT)
+	{
+		dprintf(1, "CONTINUED ... BUT EXIT LOL\n");
+		//exit(0);
+	}
+}
+
+int		main(int ac, char **av, char **envp)
+{
+	int pid;
+	int status = 0;
+	struct sigaction sga;
+
+	sigemptyset(&sga.sa_mask);
+	sga.sa_flags = 0;
+	sga.sa_handler = &sighandler;
+	sigaction(SIGSTOP, &sga, 0);
+	sigaction(SIGCONT, &sga, 0);
+
+	dprintf(1, "%d", getpid());
+	if (!(pid = fork()))
+		execve(av[1], &av[1], envp);
+	else
+	{
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			while (1)
+				if (lol)
+					write(1, "0\n", 2);
+	}
+	return 0;
+}
---
+++ TESTS/test2.c
@@ -0,0 +1,20 @@
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int ac, char **av, char **envp)
+{
+	int pid;
+	int status = 0;
+
+	kill(atoi(av[1]), SIGSTOP);
+	if (!(pid = fork()))
+		execve(av[2], &av[2], envp);
+	else
+	{
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			kill(atoi(av[1]), SIGCONT);
+	}
+	return 0;
+}
